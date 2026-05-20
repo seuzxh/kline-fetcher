ecc92480ffd3480310da453510d36e7530db1acf
seuzxh <seuzxh@163.com>
2026-05-20 20:54:37 +0800

    版本升级到 1.1.0：新增概念板块功能

--- kline_fetcher/__init__.py
+++ kline_fetcher/__init__.py
@@ -2,4 +2,4 @@ from kline_fetcher.fetcher import KLineFetcher
 from kline_fetcher.converter import KLineToQlib
 
 __all__ = ["KLineFetcher", "KLineToQlib"]
-__version__ = "1.0.0"
+__version__ = "1.1.0"
